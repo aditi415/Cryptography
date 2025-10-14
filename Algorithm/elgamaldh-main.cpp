dd94c03e425682f5909377dd957b64781b15d79e
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-10-14 14:16:43 +0530

    Add files via upload

---
+++ Algorithm/elgamaldh-main.cpp
@@ -0,0 +1,20 @@
+#include "elgamaldh.hpp"
+
+int main() {
+    ElGamalDH elgdh;
+    int choice;
+    cout << "1. Diffie-Hellman Key Exchange" << endl;
+    cout << "2. ElGamal Encryption & Decryption" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+        elgdh.diffieHellman();
+    else if (choice == 2)
+        elgdh.elGamal();
+    else
+        cout << "Invalid choice!" << endl;
+
+    return 0;
+}
+
