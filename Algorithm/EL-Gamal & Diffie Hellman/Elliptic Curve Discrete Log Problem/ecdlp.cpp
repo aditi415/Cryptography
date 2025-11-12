b99337605dcf094986571271d9745d7661da8f79
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-11-12 10:16:06 +0530

    ecdlp.cpp

--- Algorithm/EL-Gamal & Diffie Hellman/Elliptic Curve Discrete Log Problem/ecdlp.cpp
+++ Algorithm/EL-Gamal & Diffie Hellman/Elliptic Curve Discrete Log Problem/ecdlp.cpp
@@ -1,31 +1,35 @@
 #include "ecdlp.hpp"
 
-EllipticCurve::EllipticCurve(ZZ_p _a, ZZ_p _b) {
+EllipticCurve::EllipticCurve(ZZ _p, ZZ_p _a, ZZ_p _b) {
+    p = _p;
+    ZZ_p::init(p);
     a = _a;
     b = _b;
 }
 
+void EllipticCurve::printParams() {
+    cout << "Curve: y^2 = x^3 + " << a << "x + " << b << " (mod " << p << ")" << endl;
+}
+
 Point EllipticCurve::add(Point P, Point Q) {
-    if (P.inf) 
-      return Q;
-    if (Q.inf) 
-      return P;
-    if (P.x == Q.x && P.y == -Q.y) 
-      return Point(); 
+    if (P.infinity) return Q;
+    if (Q.infinity) return P;
 
     ZZ_p lambda;
+    if (P.x == Q.x && P.y == -Q.y) return Point(); // infinity
+
     if (P.x != Q.x)
         lambda = (Q.y - P.y) / (Q.x - P.x);
     else
-        lambda = (ZZ_p(3)*P.x*P.x + a) / (ZZ_p(2)*P.y);
+        lambda = (3 * P.x * P.x + a) / (2 * P.y);
 
-    ZZ_p xr = lambda*lambda - P.x - Q.x;
-    ZZ_p yr = lambda*(P.x - xr) - P.y;
-    return Point(xr, yr);
+    ZZ_p x3 = lambda * lambda - P.x - Q.x;
+    ZZ_p y3 = lambda * (P.x - x3) - P.y;
+    return Point(x3, y3);
 }
 
-Point EllipticCurve::mul(Point P, long long k) {
-    Point R;
+Point EllipticCurve::multiply(Point P, long k) {
+    Point R; // infinity
     Point Q = P;
     while (k > 0) {
         if (k & 1) R = add(R, Q);
@@ -35,3 +39,41 @@ Point EllipticCurve::mul(Point P, long long k) {
     return R;
 }
 
+// ---------------- Encryption / Decryption ----------------
+
+pair<Point, Point> EllipticCurve::encrypt(Point M, Point publicKey, Point basePoint, long k) {
+    // Cipher = (kG, M + k*PublicKey)
+    Point C1 = multiply(basePoint, k);
+    Point C2 = add(M, multiply(publicKey, k));
+    return make_pair(C1, C2);
+}
+
+Point EllipticCurve::decrypt(pair<Point, Point> cipher, long privateKey) {
+    // M = C2 - d*C1
+    Point dC1 = multiply(cipher.first, privateKey);
+    Point neg = Point(dC1.x, -dC1.y);
+    return add(cipher.second, neg);
+}
+
+// ---------------- Digital Signature ----------------
+
+pair<ZZ_p, ZZ_p> EllipticCurve::sign(ZZ_p message, long privateKey, Point basePoint, long k) {
+    Point R = multiply(basePoint, k);
+    ZZ_p r = R.x;
+    ZZ_p k_inv = inv(conv<ZZ_p>(k));
+    ZZ_p s = k_inv * (message + r * conv<ZZ_p>(privateKey));
+    return make_pair(r, s);
+}
+
+bool EllipticCurve::verify(ZZ_p message, pair<ZZ_p, ZZ_p> signature, Point publicKey, Point basePoint) {
+    ZZ_p r = signature.first;
+    ZZ_p s = signature.second;
+    ZZ_p w = inv(s);
+    ZZ_p u1 = message * w;
+    ZZ_p u2 = r * w;
+
+    Point P1 = multiply(basePoint, to_long(rep(u1)));
+    Point P2 = multiply(publicKey, to_long(rep(u2)));
+    Point X = add(P1, P2);
+    return (X.x == r);
+}
