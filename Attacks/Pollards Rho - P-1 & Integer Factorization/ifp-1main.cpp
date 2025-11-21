90fdbb663ce0872763c4962434fc6029ac85ad55
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-11-21 15:00:17 +0530

    Add files via upload

---
+++ Attacks/Pollards Rho - P-1 & Integer Factorization/ifp-1main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <NTL/ZZ.h>
+#include "ifp-1.hpp"
+
+using namespace std;
+using namespace NTL;
+
+int main() {
+    ZZ n = ZZ(143);  
+
+    cout << "Number: " << n << "\n";
+
+    Pollard P(n);
+
+    ZZ r1 = P.pollardRho();
+    if (r1 == 1)
+        cout << "Pollard Rho failed.\n";
+    else
+        cout << "Pollard Rho factor: " << r1 << " and " << n / r1 << "\n";
+
+    ZZ r2 = P.pollardPMinus1();
+    if (r2 == 1)
+        cout << "Pollard p-1 failed.\n";
+    else
+        cout << "Pollard p-1 factor: " << r2 << " and " << n / r2 << "\n";
+
+    return 0;
+}
+
