292b437770a7dda9088250db8d31d3b472ce227f
Aditi Bhosure <bhosure.as2003@gmail.com>
2026-06-26 19:48:40 +0530

    ciphers.hpp
    Removed unnecessary blank lines in the ciphers class.

--- CIPHERS/2/ciphers.hpp
+++ CIPHERS/2/ciphers.hpp
@@ -1,6 +1,5 @@
 #ifndef CIPHER_HPP
 #define CIPHER_HPP
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,15 +11,12 @@ public:
     void hill();
 
 private:
-  
     std::string caesarEncrypt(std::string text, int key);
     std::string caesarDecrypt(std::string text, int key);
 
-  
     std::string vigenereEncrypt(std::string text, std::string key);
     std::string vigenereDecrypt(std::string text, std::string key);
 
-  
     std::string hillEncrypt(std::string text, std::vector<std::vector<int>> key);
     std::string hillDecrypt(std::string text, std::vector<std::vector<int>> key);
     int modInverse(int a, int m);
